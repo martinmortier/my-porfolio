4ef5c789e91f5d4f9d220c7be1b42798f5a055ec
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2022-02-28 10:07:35 +0100

    Magic match description

--- components/CurrentProject/CurrentProject.tsx
+++ components/CurrentProject/CurrentProject.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./CurrentProject.module.css";
 import magic_card from "../../public/projects/magic_card.png";
 import planhess from "../../public/projects/planhess.png";
 import portfolio from "../../public/projects/portfolio.png";
+import { FaGithub } from "react-icons/fa";
 
 const CurrentProject = ({ currentProjectRef }: any): JSX.Element => {
   return (
@@ -11,7 +13,6 @@ const CurrentProject = ({ currentProjectRef }: any): JSX.Element => {
       <div className={styles.projectsContainer}>
         <div className={styles.leftSection}>
           <div className={styles.projectContainer}>
-            <p className={styles.projectDescription}>Test</p>
             <div className={styles.imageContainer}>
               <Image
                 src={magic_card}
@@ -21,9 +22,21 @@ const CurrentProject = ({ currentProjectRef }: any): JSX.Element => {
                 placeholder="blur"
               />
             </div>
+            <div className={styles.projectDescription}>
+              <p>Magic Match is a flip card memory game.</p>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <Link href="https://github.com/martinmortier/magic-memory">
+                  <div>
+                    <FaGithub size="2em" style={{ cursor: "pointer" }} />
+                  </div>
+                </Link>
+                <Link href="https://magic-memory-mbtakvyhl-martinmortier.vercel.app/">
+                  Play with it
+                </Link>
+              </div>
+            </div>
           </div>
           <div className={styles.projectContainer}>
-            <p className={styles.projectDescription}>Test</p>
             <div className={styles.imageContainer}>
               <Image
                 src={portfolio}
@@ -33,11 +46,26 @@ const CurrentProject = ({ currentProjectRef }: any): JSX.Element => {
                 placeholder="blur"
               />
             </div>
+            <div className={styles.projectDescription}>
+              <p>Test 2</p>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <Link href="https://www.google.fr">
+                  <FaGithub size="2em" />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
         <div className={styles.rightSection}>
           <div className={styles.projectContainer}>
-            <p className={styles.projectDescription}>Test</p>
+            <div className={styles.projectDescription}>
+              <p>Test 3</p>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <Link href="https://www.google.fr">
+                  <FaGithub size="2em" />
+                </Link>
+              </div>
+            </div>
             <div className={styles.imageContainer}>
               <Image
                 src={planhess}
